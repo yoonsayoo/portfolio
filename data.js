b926972a895f5ee3e60b0ea479756f110c684b15
yoonsayoo <114141842+yoonsayoo@users.noreply.github.com>
2026-03-21 21:52:16 +0900

    Update data.js

--- data.js
+++ data.js
@@ -16,5 +16,14 @@ const artworks = [
     material: "Oil on canvas",
     size: "145 × 112 cm",
     image: "images/stone-figure.jpg"
+  },
+  {
+    series: "self-portraits",
+    title: "늙은 민들레",
+    titleEn: "An Old Dandelion",
+    year: "2022",
+    material: "Oil on canvas",
+    size: "116 × 91 cm",
+    image: "images/old-dandelion.jpg"
   }
 ];
