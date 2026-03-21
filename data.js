1c6f654ec8b718537ccbed3f6d55659b83f0701d
yoonsayoo <114141842+yoonsayoo@users.noreply.github.com>
2026-03-21 21:02:45 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 const artworks = [
   {
     series: "people",
-    title: "긴 늪",
-    titleEn: "The Long Marsh",
+    title: "흰옷 입은 사람",
+    titleEn: "The One in White",
     year: "2024",
     material: "Oil on canvas",
     size: "130 × 97 cm",
-    image: "images/long-marsh.jpg"
+    image: "images/the-one-in-white.jpg"
   },
   {
     series: "people",
@@ -16,41 +16,5 @@ const artworks = [
     material: "Oil on canvas",
     size: "145 × 112 cm",
     image: "images/stone-figure.jpg"
-  },
-  {
-    series: "people",
-    title: "성냥개비 나무",
-    titleEn: "Matchstick Tree",
-    year: "2023",
-    material: "Oil on canvas",
-    size: "91 × 73 cm",
-    image: "images/matchstick-tree.jpg"
-  },
-  {
-    series: "self-portraits",
-    title: "늙은 민들레",
-    titleEn: "An Old Dandelion",
-    year: "2022",
-    material: "Oil on canvas",
-    size: "116 × 91 cm",
-    image: "images/old-dandelion.jpg"
-  },
-  {
-    series: "self-portraits",
-    title: "당신의 차가운 심장",
-    titleEn: "Your Cold Heart",
-    year: "2022",
-    material: "Oil on canvas",
-    size: "100 × 80 cm",
-    image: "images/your-cold-heart.jpg"
-  },
-  {
-    series: "roots",
-    title: "물고기 계단",
-    titleEn: "Fish Stairs",
-    year: "2021",
-    material: "Oil on canvas",
-    size: "162 × 130 cm",
-    image: "images/fish-stairs.jpg"
   }
 ];
